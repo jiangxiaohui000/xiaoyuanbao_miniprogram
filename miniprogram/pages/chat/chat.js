71211711db5f9a636e6b5af937aabac44d09bbb5
jiangxiaohui <18363671622@163.com>
2020-07-24 23:07:27 +0800

    开发聊天界面

--- miniprogram/pages/chat/chat.js
+++ miniprogram/pages/chat/chat.js
@@ -3,7 +3,16 @@ const app = getApp();
 Page({
     data: {
         chatList: [{
-            
+            id: 1,
+            name: '快乐的小甜甜',
+            info: '便宜点吧，太贵了，便宜点吧，太贵了，便宜点吧，太贵了，便宜点吧，太贵了，便宜点吧，太贵了，便宜点吧，太贵了',
+            img: '../../images/touxiang1.jpeg',
+
+        }, {
+            id: 2,
+            name: '雨中追逐',
+            info: '项目中常常有这种需要我们对溢出文本进行"..."显示的操作，单行多行的情况都有',
+            img: '../../images/touxiang2.jpeg',
         }]
     }
 })
