d1bdd025c6c4c12120537b3eaea2688f4e6633a6
jiangxiaohui <jiangxiaohui7178@navinfo.com>
2020-12-06 17:27:55 +0800

    首页开发

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -13,7 +13,59 @@ Page({
       desc: '假数据的覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
       currentPrice: '111',
       originPrice: '2343'
-    }]
+    }, {
+      id: 2,
+      img: '../../images/touxiang2.jpeg',
+      desc: '刻录机阿空手道假数据的覅哦啊阿斯殴打见覅见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '33333',
+      originPrice: '23222243'
+    }, {
+      id: 3,
+      img: '../../images/touxiang1.jpeg',
+      desc: '假数据的覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '111',
+      originPrice: '2343'
+    }, {
+      id: 4,
+      img: '../../images/touxiang2.jpeg',
+      desc: '刻录机阿空手道假数据的覅哦啊阿斯殴打见覅见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '33333',
+      originPrice: '23222243'
+    }, {
+      id: 5,
+      img: '../../images/touxiang1.jpeg',
+      desc: '假数据的覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '111',
+      originPrice: '2343'
+    }, {
+      id: 6,
+      img: '../../images/touxiang2.jpeg',
+      desc: '刻录机阿空手道假数据的覅哦啊阿斯殴打见覅见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '33333',
+      originPrice: '23222243'
+    }, {
+      id: 7,
+      img: '../../images/touxiang1.jpeg',
+      desc: '假数据的覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅哦啊阿斯殴打见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '111',
+      originPrice: '2343'
+    }, {
+      id: 8,
+      img: '../../images/touxiang2.jpeg',
+      desc: '刻录机阿空手道假数据的覅哦啊阿斯殴打见覅见覅偶啊囧死的佛家是我OA水浇地偶发iOS',
+      currentPrice: '33333',
+      originPrice: '23222243'
+    }],
+    swiperImgs: [{
+			id: 1,
+			img: '../../images/banner1.jpg'
+		}, {
+			id: 2,
+			img: '../../images/banner2.jpg'
+		}, {
+			id: 3,
+			img: '../../images/banner3.jpg'
+		}]
   },
 
   onLoad: function() {
@@ -41,8 +93,16 @@ Page({
       }
     })
   },
-
 	onPullDownRefresh: function() {
 		console.log('index');
-	},
+  },
+  gotoGoodsDetail(e) {
+    console.log('detail', e.currentTarget.dataset);
+    wx.navigateTo({
+      url: '../productDetail/productDetail',
+    })
+  },
+  gotoCurrentChat(e) {
+    console.log('chat', e.currentTarget.dataset);
+  }
 })
