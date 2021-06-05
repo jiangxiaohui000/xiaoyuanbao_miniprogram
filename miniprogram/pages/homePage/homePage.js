8c24cbaeb61879b5ccbbda957850b64949dcff98
jiangxiaohui <jiangxiaohui7178@navinfo.com>
2021-06-05 16:09:17 +0800

    调整页面

--- miniprogram/pages/homePage/homePage.js
+++ miniprogram/pages/homePage/homePage.js
@@ -79,6 +79,8 @@ Page({
     locationFlash: true,
     locationShow: false,
     timer: null,
+    avatarUrl: '',
+    userInfo: '',
   },
 
   onLoad: function() {
@@ -88,21 +90,13 @@ Page({
       });
       return;
     }
-    // 获取用户信息
     wx.getSetting({
       success: res => {
         console.log(res, 'getSetting');
         // 账户信息
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
+          this.getUserInfo();
         }
         // 位置信息
         if(res.authSetting['scope.userLocation']) {
@@ -215,6 +209,17 @@ Page({
       }
     });
   },
+  // 获取用户信息
+  getUserInfo: function() {
+    wx.getUserInfo({
+      success: res => {
+        this.setData({
+          avatarUrl: res.userInfo.avatarUrl,
+          userInfo: res.userInfo
+        })
+      }
+    })
+  },
   // 下拉刷新
 	onPullDownRefresh: function() {
     setTimeout(() => {
@@ -272,5 +277,48 @@ Page({
   // 去往定位页面
   goLocationPage() {
     //
-  }
+  },
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        wx.showLoading({
+          title: '上传中',
+        });
+        const filePath = res.tempFilePaths[0]
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
 })
